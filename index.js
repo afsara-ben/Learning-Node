209eccabd4b3ea537de97782497bdfeacf60229c
afsara-ben <1505118.ab@ugrad.cse.buet.ac.bd>
2021-05-05 11:49:21 +0600

    learnt event module

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // const person = require('./person');
 // console.log(person.name);
 
-const Person = require('./person');
-// import Person from './person'; -- not yet implemented in node
+// const Person = require('./person');
+// // import Person from './person'; -- not yet implemented in node
 
-const person1 = new Person('Afsara benazir', 10);
-person1.greeting();
+// const person1 = new Person('Afsara benazir', 10);
+// person1.greeting();
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+logger.log("hello world");
